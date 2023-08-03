e98e0a9078485c007f4401b25d4ce3f6284cba4b
Shunshun94 <shunshun9460@gmail.com>
2023-08-04 00:10:28 +0900

    respond to some couner cases

--- sw2ytsheetQuickerLogs/script.js
+++ sw2ytsheetQuickerLogs/script.js
@@ -5,13 +5,13 @@ const base = document.getElementById('making-posts');
 base.style = 'position:relative;';
 
 const olderLink = document.createElement('a');
-olderLink.setAttribute('href', location.href.replace(currentValueExecResult[1], currentValueAsNumber - 1));
+olderLink.setAttribute('href', `https://yutorize.2-d.jp/ytsheet/sw2.5/?mode=making&num=${currentValueAsNumber - 1}`);
 olderLink.textContent = '＞';
 olderLink.style = "position:absolute;font-size:3em;right:1em;top:4em;";
 base.append(olderLink);
 
 const newerLink = document.createElement('a');
-newerLink.setAttribute('href', location.href.replace(currentValueExecResult[1], currentValueAsNumber + 1));
+newerLink.setAttribute('href', `https://yutorize.2-d.jp/ytsheet/sw2.5/?mode=making&num=${currentValueAsNumber + 1}`);
 newerLink.textContent = '＜';
 newerLink.style = "position:absolute;font-size:3em;left:1em;top:4em;";
 base.append(newerLink);
