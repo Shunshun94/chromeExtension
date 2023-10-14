32e4076bcb1f168ce0e61dfdc0db6fe86c04c2d6
Shunshun94 <shunshun9460@gmail.com>
2023-10-15 00:09:53 +0900

    update ytsheetQuickMakeLog

--- sw2ytsheetQuickerLogs/script.js
+++ sw2ytsheetQuickerLogs/script.js
@@ -5,14 +5,21 @@ const base = document.getElementById('making-posts');
 base.style = 'position:relative;';
 
 const olderLink = document.createElement('a');
-olderLink.setAttribute('href', `https://yutorize.2-d.jp/ytsheet/sw2.5/?mode=making&num=${currentValueAsNumber - 1}`);
-olderLink.textContent = '＞';
-olderLink.style = "position:absolute;font-size:3em;right:1em;top:4em;";
+olderLink.setAttribute('href', `./?mode=making&num=${currentValueAsNumber - 1}`);
+olderLink.style = "outline: 3px double #000; transform: rotate(45deg);background: linear-gradient(-45deg, #679, #cdf);position: absolute; font-size: 2.5em; right: 1em; top: 4em;width:1.2em;height:1.2em;text-align:right;color:white;";
+olderLink.title = "古いダイス結果へ";
+const olderText = document.createElement('span');
+olderText.textContent = '→';
+olderText.style = "display: block;transform: rotate(-45deg);";
+olderLink.append(olderText);
 base.append(olderLink);
 
 const newerLink = document.createElement('a');
-newerLink.setAttribute('href', `https://yutorize.2-d.jp/ytsheet/sw2.5/?mode=making&num=${currentValueAsNumber + 1}`);
-newerLink.textContent = '＜';
-newerLink.style = "position:absolute;font-size:3em;left:1em;top:4em;";
+newerLink.setAttribute('href', `./?mode=making&num=${currentValueAsNumber + 1}`);
+newerLink.style = "outline: 3px double #000; transform: rotate(-45deg);background: linear-gradient(-45deg, #679, #cdf);position: absolute; font-size: 2.5em; left: 1em; top: 4em;width:1.2em;height:1.2em;text-align:left;color:white;";
+newerLink.title = "新しいダイス結果へ";
+const newerText = document.createElement('span');
+newerText.textContent = '←';
+newerText.style = "display: block; transform: rotate(45deg);";
+newerLink.append(newerText);
 base.append(newerLink);
-
